ccbd37ae5468ed820903e0c6bee3839d3cd88d6e
badingaguyde-glitch <badingaguyde@gmail.com>
2026-07-07 10:30:40 +0300

    Add MongoDB context and configuration
    - Implement MongoDBContext for database interactions.
    - Add MongoDB connection string to .env file.
    - Register MongoDBContext as a singleton service in Program.cs.
    - Update .gitignore to include .env file.

--- StockManager.Server/Program.cs
+++ StockManager.Server/Program.cs
@@ -1,7 +1,11 @@
+using StockManager.Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<MongoDBContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
