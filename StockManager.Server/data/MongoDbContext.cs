669f527409accf93144582963304bf7b98b63e6f
badingaguyde-glitch <badingaguyde@gmail.com>
2026-07-10 12:26:26 +0300

    Add Notification and AuditLog collections to MongoDB context

--- StockManager.Server/data/MongoDbContext.cs
+++ StockManager.Server/data/MongoDbContext.cs
@@ -26,6 +26,8 @@ namespace StockManager.Server.Data
         public IMongoCollection<StockMovement> StockMovements => _database.GetCollection<StockMovement>("StockMovements");
         public IMongoCollection<Sale> Sales => _database.GetCollection<Sale>("Sales");
         public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
+        public IMongoCollection<Notification> Notifications => _database.GetCollection<Notification>("Notifications");
+        public IMongoCollection<AuditLog> AuditLogs => _database.GetCollection<AuditLog>("AuditLogs");
         
 
         private void SeedData()
