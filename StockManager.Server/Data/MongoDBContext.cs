1c8cce575ed848dcdbfcf0b27ce940599cdf0a36
badingaguyde-glitch <badingaguyde@gmail.com>
2026-07-10 11:37:06 +0300

    Add audit logging functionality for product and sales actions

--- StockManager.Server/Data/MongoDBContext.cs
+++ StockManager.Server/Data/MongoDBContext.cs
@@ -27,6 +27,7 @@ namespace StockManager.Server.Data
         public IMongoCollection<Sale> Sales => _database.GetCollection<Sale>("Sales");
         public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
         public IMongoCollection<Notification> Notifications => _database.GetCollection<Notification>("Notifications");
+        public IMongoCollection<AuditLog> AuditLogs => _database.GetCollection<AuditLog>("AuditLogs");
         
 
         private void SeedData()
